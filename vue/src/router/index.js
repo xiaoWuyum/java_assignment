7b77a54708b910439b0f7cbadaee99eb1c98d84a
xiaoWuyum <105533925+xiaoWuyum@users.noreply.github.com>
2022-07-03 17:43:08 +0800

    Add files via upload

---
+++ vue/src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import store from "@/store";
+
+Vue.use(VueRouter)
+
+
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('../views/Manage.vue'),
+    redirect: "/home",
+    children: [
+      { path: 'home', name: '首页', component: () => import('../views/HomeView.vue')},
+      { path: 'user', name: '用户管理', component: () => import('../views/User.vue')},
+      { path: 'device', name: '设备管理', component: () => import('../views/Device.vue')},
+      { path: 'check', name: '检查管理', component: () => import('../views/Check.vue')},
+    ]
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/Register.vue')
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+// 路由守卫
+router.beforeEach((to, from, next) => {
+  localStorage.setItem("currentPathName", to.name)  // 设置当前的路由名称，为了在Header组件中去使用
+  store.commit("setPath")  // 触发store的数据更新
+  next()  // 放行路由
+})
+
+
+export default router
