98b8593a30bc850f5e8b9f5a8e3c658f8912053a
xiaoWuyum <105533925+xiaoWuyum@users.noreply.github.com>
2022-07-03 17:50:43 +0800

    Add files via upload

---
+++ hou/src/main/java/com/yun/hou/SApplication.java
@@ -0,0 +1,15 @@
+package com.yun.hou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SApplication.class, args);
+    }
+
+}
