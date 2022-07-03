bb5de0fe815a54ed137127ef38fcdb5d48ae75d9
xiaoWuyum <105533925+xiaoWuyum@users.noreply.github.com>
2022-07-03 17:44:33 +0800

    Add files via upload

---
+++ vue/src/components/Aside.vue
@@ -0,0 +1,56 @@
+<template>
+  <el-menu :default-openeds="['1', '3']" style="min-height: 100%; overflow-x: hidden"
+           background-color="#006400"
+           text-color="#fff"
+           active-text-color="#ffd04b"
+           :collapse-transition="false"
+           :collapse="isCollapse"
+           router
+  >
+    <div style="height: 60px; line-height: 60px; text-align: center">
+      <img src="../assets/01.png" alt="" style="width: 20px; position: relative; top: 5px; right: 5px">
+      <b style="color: white" v-show="logoTextShow">消防设备管理系统</b>
+    </div>
+    <el-menu-item index="/home">
+      <template slot="title">
+        <i class="el-icon-house"></i>
+        <span slot="title">主页</span>
+      </template>
+    </el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">
+        <i class="el-icon-menu"></i>
+        <span slot="title">系统管理</span>
+      </template>
+
+      <el-menu-item index="/user">
+        <i class="el-icon-s-custom"></i>
+        <span slot="title">用户管理</span>
+      </el-menu-item>
+
+      <el-menu-item index="/device">
+        <i class="el-icon-document"></i>
+        <span slot="title">设备管理</span>
+      </el-menu-item>
+
+      <el-menu-item index="/check">
+        <i class="el-icon-setting"></i>
+        <span slot="title">检查管理</span>
+      </el-menu-item>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "Aside",
+  props: {
+    isCollapse: Boolean,
+    logoTextShow: Boolean
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
